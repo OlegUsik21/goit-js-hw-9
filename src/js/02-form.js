bf323f13080249822f3918bc7681057d41cedd3a
OlegUsik21 <Oleg2Usik1@gmail.com>
2024-06-03 13:23:59 +0200

    homework

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,1 +1,41 @@
-console.log("form");
+const form = document.querySelector('.feedback-form');
+const formData = { email: "", message: "" };
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedData = loadFromLS('feedback-form-state') || {};
+    form.elements.email.value = savedData.email || '';
+    form.elements.message.value = savedData.message || '';
+    formData.email = savedData.email || '';
+    formData.message = savedData.message || '';
+});
+
+form.addEventListener('input', () => {
+    formData.email = form.elements.email.value.trim();
+    formData.message = form.elements.message.value.trim();
+    saveToLS('feedback-form-state', formData);
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!formData.email || !formData.message) {
+        alert('Please fill all fields');
+        return;
+    }
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+    formData.email = '';
+    formData.message = '';
+});
+
+function saveToLS(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function loadFromLS(key) {
+    try {
+        return JSON.parse(localStorage.getItem(key));
+    } catch {
+        return null;
+    }
+}
